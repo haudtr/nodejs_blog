1a2326ef363cf910d8b3c2c2b89081f5ddc6b995
haudtr285 <thehauhuynh5@gmail.com>
2022-11-23 13:16:35 +0700

    husky

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine(
   'hbs',
-        handlebars.engine({
+          handlebars.engine({
     extname: '.hbs',
   }),
 );
